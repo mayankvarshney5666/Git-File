72f2950de343f50fcc8b16decb343c5ce61d9248
Mayank Varshney <tikaramvarshney566@gmail.com>
2022-04-22 19:46:29 +0530

    multiplication file

---
+++ multi.java
@@ -0,0 +1,9 @@
+public class multi {
+
+    public static void main(String[] args) {
+        int b;
+        int a = b = 10;
+        int c = a * b;
+        System.out.println("This is Multiply of a&b" + c);
+    }
+}
