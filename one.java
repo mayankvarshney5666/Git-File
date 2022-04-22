bf5b86eb7156b4b6889f9592d842c55ef1c26376
Mayank Varshney <tikaramvarshney566@gmail.com>
2022-04-22 19:23:11 +0530

    completion file code

---
+++ one.java
@@ -0,0 +1,12 @@
+/**
+ * one
+ */
+public class one {
+
+    public static void main(String[] args) {
+        int b;
+        int a = b = 10;
+        int c = a + b;
+        System.out.println("This is sum of a&b" + c);
+    }
+}
